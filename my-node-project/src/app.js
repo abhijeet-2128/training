54010deab72be054896871eb41733b076a707f08
abhijeet-2128 <abhijeet.srivastava@appinventiv.com>
2023-07-07 17:55:37 +0530

    fs upload read

--- my-node-project/src/app.js
+++ my-node-project/src/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()  
 
 //importing the routes
 const userRoutes = require('./routes/userRoutes');
 const home = require('./routes/home');
 const calculator = require('./routes/calculator');
-//use routes
 
+//use routes
 app.use(home);
 app.use(calculator);
 app.use(userRoutes);
 
+//access env variables
+const dbport = process.env.DB_PORT
+const dbhost = process.env.DB_HOST
 
 
 app.listen(3000,()=>{
