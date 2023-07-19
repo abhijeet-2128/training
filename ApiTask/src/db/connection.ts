0ba5bb90a4c3424d4de6abd717ba0c6e31f550e9
abhijeet-2128 <abhijeet.srivastava@appinventiv.com>
2023-07-19 12:44:36 +0530

    Removed All folders

--- ApiTask/src/db/connection.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('test_postgres', 'postgres', '      ', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-export default sequelize;
